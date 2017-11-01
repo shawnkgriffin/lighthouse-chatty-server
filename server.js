bb4015f611d162e747acb370be492a964a3c0669
Shawn Griffin <shawn@shawngriffin.com>
2017-11-01 23:30:39 +0000

    added uuidv4 to message

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const uuidv4 = require('uuid/v4');
 const express = require('express');
 const http = require('http');
 const ws = require('ws');
@@ -6,26 +7,49 @@ const app = express();
 
 app.use(express.static('public'));
 
-const server = http.createServer(app);
+// Set the port to 3001
+const PORT = 3001;
+
+// Create a new express server
+const server = express()
+   // Make the express server serve static assets (html, javascript, css) from the /public folder
+  .use(express.static('public'))
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+
 // const wss = new ws.Server({server});
 const wss = new ws.Server({server: server});
 
 function broadcast(data) {
   wss.clients.forEach((client) => {
+
     if (client.readyState === ws.OPEN) {
+      console.log('broadcast', data);
       client.send(data);
     }
   });
 }
 
+/**
+* FUNCTION 
+* Take in a JSON encoded message from someone, parse it, add a UUID and return it as a message.
+* @function getUsers
+* @param {string} data - JSON encoded string from a user. contains username, content.
+* @param {function} callback 
+* @returns {object} Users = array of Users
+*/
+
+function buildMessage(data) {
+  message = JSON.parse(data);
+  message.id = uuidv4();
+  console.log('buildMessage', message)
+  return message;
+}
+
 wss.on('connection', (socket) => {
-
+  console.log('connection');
   socket.on('message', (data) => {
-    broadcast(data);
+    console.log('message',data);
+    const message = buildMessage(data);
+    broadcast(JSON.stringify(message));
   });
-
-});
-
-server.listen(8081, function listening() {
-  console.log('Listening on %d', server.address().port);
 });
